262437bb20d2a8cb55c3a2a65b498b8426d4393f
macantonioroya <135991097+macantonioroya@users.noreply.github.com>
2023-06-15 14:53:27 +0000

    implementacion de modulos

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # tallermarco
+## Taller Nro 1 Entorno de desarrollo
+Nombre: Marco Rojas
+
+
+## Descripcion
+## Funcionamiento
+
+
+![](img/thunderstorm-gd0d93f437_1280.jpg)
---
+++ ptograma.py
@@ -0,0 +1,12 @@
+import matematicas
+
+print(matematicas.sumar (2,3))
+
+from matematicas import sumar, restar, multiplicar, dividir
+from algebra import distancia
+
+print(sumar(2,3))
+print(restar(4,2))
+print(multiplicar(8,4))
+print(dividir(10,2))
+print(distancia(1, 2, 3, 4))
